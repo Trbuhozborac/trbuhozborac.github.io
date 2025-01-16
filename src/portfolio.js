a8be32d31bde82344a61789693078e01b6c5d1d8
trbuhozborac <nemanjapipi@hotmail.com>
2025-01-16 12:21:04 +0100

    initial portfolio version

--- src/portfolio.js
+++ src/portfolio.js
@@ -20,8 +20,8 @@ const illustration = {
 };
 
 const greeting = {
-  username: "Saad Pasta",
-  title: "Hi all, I'm Saad",
+  username: "Trbuhozborac",
+  title: "Hi all, I'm Nemanja Pilipović aka Trbuhozborac",
   subTitle: emoji(
     "A passionate Full Stack Software Developer 🚀 having an experience of building Web and Mobile applications with JavaScript / Reactjs / Nodejs / React Native and some other cool libraries and frameworks."
   ),
@@ -33,13 +33,13 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
-  linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
-  gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
-  medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
+  github: "https://github.com/trbuhozborac",
+  linkedin: "https://www.linkedin.com/in/nemanja-pilipovic/",
+  gmail: "nemanjapilip@gmail.com",
+  gitlab: "https://gitlab.com/trbuhozborac",
+  // facebook: "https://www.facebook.com/saad.pasta7",
+  medium: "https://medium.com/@trbuhozborac",
+  stackoverflow: "https://stackoverflow.com/users/9711575/nemanja-pilipovi%c4%87",
   // Instagram, Twitter and Kaggle are also supported in the links!
   // To customize icons and social links, tweak src/components/SocialMedia
   display: true // Set true to display this section, defaults to false
@@ -56,7 +56,7 @@ const skillsSection = {
     ),
     emoji("⚡ Progressive Web Applications ( PWA ) in normal and SPA Stacks"),
     emoji(
-      "⚡ Integration of third party services such as Firebase/ AWS / Digital Ocean"
+      "⚡ Integration of third party services such as Firebase / AWS / Digital Ocean"
     )
   ],
 
@@ -64,22 +64,6 @@ const skillsSection = {
 https://fontawesome.com/icons?d=gallery */
 
   softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
     {
       skillName: "reactjs",
       fontAwesomeClassname: "fab fa-react"
@@ -89,33 +73,33 @@ https://fontawesome.com/icons?d=gallery */
       fontAwesomeClassname: "fab fa-node"
     },
     {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
+      skillName: "JavaScript",
+      fontAwesomeClassname: "fab fa-js"
     },
     {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
+      skillName: "html-5",
+      fontAwesomeClassname: "fab fa-html5"
+    },
+    {
+      skillName: "css3",
+      fontAwesomeClassname: "fab fa-css3-alt"
     },
     {
       skillName: "sql-database",
       fontAwesomeClassname: "fas fa-database"
     },
     {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
       skillName: "docker",
       fontAwesomeClassname: "fab fa-docker"
-    }
+    },
+    {
+      skillName: "npm",
+      fontAwesomeClassname: "fab fa-npm"
+    },
+    {
+      skillName: "git",
+      fontAwesomeClassname: "fab fa-git"
+    },
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -126,23 +110,18 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
-      desc: "Participated in the research of XXX and published 3 papers.",
-      descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
-      ]
+      schoolName: "Higher technical school Novi Sad",
+      logo: require("./assets/images/vtsLogo.png"),
+      subHeader: "Information technologies",
+      duration: "October 2015 - May 2018",
+      desc: "Novi Sad",
     },
     {
-      schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
-      subHeader: "Bachelor of Science in Computer Science",
-      duration: "September 2013 - April 2017",
-      desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
-      descBullets: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit"]
+      schoolName: "High school „22. Oktobar“",
+      logo: require("./assets/images/srednjaLogo.png"),
+      subHeader: "Economic technician",
+      duration: "September 2011 - June 2015",
+      desc: "Žabalj"     
     }
   ]
 };
@@ -153,17 +132,17 @@ const techStack = {
   viewSkillBars: true, //Set it to true to show Proficiency Section
   experience: [
     {
-      Stack: "Frontend/Design", //Insert stack or technology you have experience in
+      Stack: "Frontend", //Insert stack or technology you have experience in
       progressPercentage: "90%" //Insert relative proficiency in percentage
     },
     {
       Stack: "Backend",
-      progressPercentage: "70%"
+      progressPercentage: "75%"
     },
     {
-      Stack: "Programming",
-      progressPercentage: "60%"
-    }
+      Stack: "UI / UX",
+      progressPercentage: "70%"
+    },
   ],
   displayCodersrank: false // Set true to display codersrank badges section need to changes your username in src/containers/skillProgress/skillProgress.js:17:62, defaults to false
 };
@@ -174,30 +153,49 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Engineer",
-      company: "Facebook",
-      companylogo: require("./assets/images/facebookLogo.png"),
-      date: "June 2018 – Present",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      role: "Front-End Developer",
+      company: "Smile AI",
+      companylogo: require("./assets/images/smileLogo.png"),
+      date: "Mar 2024 – Present",
       descBullets: [
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit"
+        `Led the implementation of AI-driven automation initiatives, integrating OpenAI APIs and AI
+         agents, while experimenting with emerging technologies in a hands-on manner.`,
+        `Served as the sole developer on multiple internal projects—independently creating new
+         applications from scratch and maintaining two core systems.`,
+        `Oversaw the full software development lifecycle, leveraging React.js on the front-end, NestJS
+         on the back-end, and handling database management and deployment tasks single-handedly.`
       ]
     },
     {
-      role: "Front-End Developer",
-      company: "Quora",
-      companylogo: require("./assets/images/quoraLogo.png"),
-      date: "May 2017 – May 2018",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      role: "Front-end developer",
+      company: "SmaRF Solutions",
+      companylogo: require("./assets/images/smarfLogo.png"),
+      date: "Jun 2015 – Feb 2024",
+      descBullets: [
+        `Spearheaded the development of responsive and reusable components using React.js, ensuring
+         a seamless and user-friendly frontend experience.`,
+        `Executed backend tasks proﬁciently using C#, where I established API routes and seamlessly
+         integrated them into the frontend. Implemented new database tables using Entity Framework
+         Core and developed the requisite infrastructure utilizing the repository pattern.`,
+        `Successfully developed, integrated and deployed two Shopify web shops with our internal
+         company application using a Node.js application.`
+      ]
     },
     {
-      role: "Software Engineer Intern",
-      company: "Airbnb",
-      companylogo: require("./assets/images/airbnbLogo.png"),
-      date: "Jan 2015 – Sep 2015",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    }
+      role: "Full-stack Developer",
+      company: "Croative",
+      companylogo: require("./assets/images/croativeLogo.png"),
+      date: "Nov 2018 – May 2022",
+      descBullets: [
+        `Played a pivotal role in enhancing an existing MERN (MongoDB, Express, React, Node)
+         application by introducing new features and meticulously addressing bugs, showcasing a
+         commitment to continuous improvement and functionality reﬁnement.`,
+        `Demonstrated agility in troubleshooting and resolving production bugs in a live React Native
+         mobile application, ensuring seamless user experiences and application reliability.`,
+        `Led the end-to-end development of a dynamic boat renting application, utilizing React for the
+         front-end and Node.js for the back-end, showcasing proﬁciency in full-stack development.`
+      ]
+    },  
   ]
 };
 
@@ -206,7 +204,7 @@ To know how to get github key look at readme.md */
 
 const openSource = {
   showGithubProfile: "true", // Set true or false to show Contact profile using Github, defaults to true
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Some big projects you have worked on
@@ -239,7 +237,7 @@ const bigProjects = {
       ]
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Achievement Section
@@ -252,53 +250,135 @@ const achievementSection = {
 
   achievementsCards: [
     {
-      title: "Google Code-In Finalist",
+      title: "AI Engineering Specialization",
       subtitle:
-        "First Pakistani to be selected as Google Code-in Finalist from 4000 students from 77 different countries.",
-      image: require("./assets/images/codeInLogo.webp"),
-      imageAlt: "Google Code-In Logo",
+        "Scrimba",
+      image: require("./assets/images/scrimbaLogo.png"),
+      imageAlt: "Scrimba Logo",
       footerLink: [
         {
           name: "Certification",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dYkVvNjdNWjNybWJrbndFSHpNY2NFV1p4YmU0/view?usp=sharing"
+          url: "https://www.coursera.org/account/accomplishments/specialization/4AM3AKR2H4MY"
         },
-        {
-          name: "Award Letter",
-          url: "https://drive.google.com/file/d/0B7kazrtMwm5dekxBTW5hQkg2WXUyR3QzQmR0VERiLXlGRVdF/view?usp=sharing"
-        },
-        {
-          name: "Google Code-in Blog",
-          url: "https://opensource.googleblog.com/2019/01/google-code-in-2018-winners.html"
-        }
       ]
     },
     {
-      title: "Google Assistant Action",
+      title: "Build AI Apps with LangChain.js",
       subtitle:
-        "Developed a Google Assistant Action JavaScript Guru that is available on 2 Billion devices world wide.",
-      image: require("./assets/images/googleAssistantLogo.webp"),
-      imageAlt: "Google Assistant Action Logo",
+        "Scrimba",
+      image: require("./assets/images/scrimbaLogo.png"),
+      imageAlt: "Scrimba Logo",
       footerLink: [
         {
-          name: "View Google Assistant Action",
-          url: "https://assistant.google.com/services/a/uid/000000100ee688ee?hl=en"
-        }
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/records/7WJUAQW3HZMB"
+        },
       ]
     },
-
     {
-      title: "PWA Web App Developer",
-      subtitle: "Completed Certifcation from SMIT for PWA Web App Development",
-      image: require("./assets/images/pwaLogo.webp"),
-      imageAlt: "PWA Logo",
+      title: "Learn AI Agents",
+      subtitle:
+        "Scrimba",
+      image: require("./assets/images/scrimbaLogo.png"),
+      imageAlt: "Scrimba Logo",
       footerLink: [
-        {name: "Certification", url: ""},
         {
-          name: "Final Project",
-          url: "https://pakistan-olx-1.firebaseapp.com/"
-        }
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/records/9ZVW3PHBCGAR"
+        },
       ]
-    }
+    },
+    {
+      title: "Learn Embeddings and Vector Databases",
+      subtitle:
+        "Scrimba",
+      image: require("./assets/images/scrimbaLogo.png"),
+      imageAlt: "Scrimba Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/records/TMGU8PCL49EV"
+        },
+      ]
+    },
+    {
+      title: "Open-source AI Models",
+      subtitle:
+        "Scrimba",
+      image: require("./assets/images/scrimbaLogo.png"),
+      imageAlt: "Scrimba Logo",
+      footerLink: [
+        {
+          name: "Certification",
+          url: "https://www.coursera.org/account/accomplishments/records/2UBFLCVGQTF6"
+        },
+      ]
+    },
+    {
+      title: "Complete React Native in 2021: Zero to Mastery",
+      subtitle:
+        "Udemy",
+      image: require("./assets/images/udemyLogo.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Course URL",
+          url: "https://www.udemy.com/course/complete-react-native-course-from-zero-to-mastery/"
+        },
+      ]
+    },
+    {
+      title: "Node with React: Fullstack Web Development",
+      subtitle:
+        "Udemy",
+      image: require("./assets/images/udemyLogo.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Course URL",
+          url: "https://www.udemy.com/course/node-with-react-fullstack-web-development/"
+        },
+      ]
+    },
+    {
+      title: "Advanced CSS and Sass: Flexbox, Grid, Animations and More!",
+      subtitle:
+        "Udemy",
+      image: require("./assets/images/udemyLogo.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Course URL",
+          url: "https://www.udemy.com/course/advanced-css-and-sass/"
+        },
+      ]
+    },
+    {
+      title: "The Complete ASP.NET MVC 5 Course",
+      subtitle:
+        "Udemy",
+      image: require("./assets/images/udemyLogo.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Course URL",
+          url: "https://www.udemy.com/course/the-complete-aspnet-mvc-5-course/"
+        },
+      ]
+    },
+    {
+      title: "Learn To Use APIs With Visual Studio, C#, ASP.Net",
+      subtitle:
+        "Udemy",
+      image: require("./assets/images/udemyLogo.png"),
+      imageAlt: "Udemy Logo",
+      footerLink: [
+        {
+          name: "Course URL",
+          url: "https://www.udemy.com/course/learn-to-use-apis-with-visual-studio-c-aspnet/"
+        },
+      ]
+    },
   ],
   display: true // Set false to hide this section, defaults to true
 };
@@ -309,19 +389,19 @@ const blogSection = {
   title: "Blogs",
   subtitle:
     "With Love for Developing cool stuff, I love to write and teach others what I have learnt.",
-  displayMediumBlogs: "true", // Set true to display fetched medium blogs instead of hardcoded ones
+  displayMediumBlogs: "false", // Set true to display fetched medium blogs instead of hardcoded ones
   blogs: [
     {
-      url: "https://blog.usejournal.com/create-a-google-assistant-action-and-win-a-google-t-shirt-and-cloud-credits-4a8d86d76eae",
-      title: "Win a Google Assistant Tshirt and $200 in Google Cloud Credits",
+      url: "https://smarfsolutions.com/login-form-with-mui-and-formik/",
+      title: "Creating a Login Form with MUI and Formik in a React Application",
       description:
-        "Do you want to win $200 and Google Assistant Tshirt by creating a Google Assistant Action in less then 30 min?"
+        "In this article, we’ll demonstrate how to create a login form in a React application using the Material User Interface (MUI) and Formik libraries."
     },
     {
-      url: "https://medium.com/@saadpasta/why-react-is-the-best-5a97563f423e",
-      title: "Why REACT is The Best?",
+      url: "https://smarfsolutions.com/fluent-validation/",
+      title: "Fluent Validation in ASP.NET Core",
       description:
-        "React is a JavaScript library for building User Interface. It is maintained by Facebook and a community of individual developers and companies."
+        "In this article, we’ll demonstrate how to use Fluent Validation in ASP.NET Core to validate incoming requests."
     }
   ],
   display: true // Set false to hide this section, defaults to true
@@ -343,7 +423,7 @@ const talkSection = {
       event_url: "https://www.facebook.com/events/2339906106275053/"
     }
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Podcast Section
@@ -356,7 +436,7 @@ const podcastSection = {
   podcast: [
     "https://anchor.fm/codevcast/embed/episodes/DevStory---Saad-Pasta-from-Karachi--Pakistan-e9givv/a-a15itvo"
   ],
-  display: true // Set false to hide this section, defaults to true
+  display: false // Set false to hide this section, defaults to true
 };
 
 // Resume Section
@@ -372,15 +452,15 @@ const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle:
     "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+92-0000000000",
-  email_address: "saadpasta70@gmail.com"
+  number: "+381-655037556",
+  email_address: "nemanjapilip@gmail.com"
 };
 
 // Twitter Section
 
 const twitterDetails = {
   userName: "twitter", //Replace "twitter" with your twitter username without @
-  display: true // Set true to display this section, defaults to false
+  display: false // Set true to display this section, defaults to false
 };
 
 const isHireable = false; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
