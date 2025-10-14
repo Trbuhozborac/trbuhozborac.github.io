606de363563c7854afa55671f58c9ec60a7815c3
trbuhozborac <nemanjapipi@hotmail.com>
2025-10-14 21:01:00 +0200

    added analyitics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from "react";
-import "./App.scss";
+import ReactGA from "react-ga4";
 import Main from "./containers/Main";
+import {useEffect} from "react";
+import "./App.scss";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-C6V8XZX5C4");
+    ReactGA.send("pageview");
+  }, []);
+
   return (
     <div>
       <Main />
